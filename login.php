25c0656c8045a945ca4e5df70859e5e7f701e7e2
Richard Marcell <richard.marcell8888@gmail.com>
2023-03-27 16:54:09 +0700

    Membuat Login dan Sign Up page serta membuat nya responsif dan dapat mengirim data ke database serta memeperbaiki bug di landing page

---
+++ login.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+// Inisialisasi Database
+$db = mysqli_connect("localhost", "root", "", "kelontongers");
+
+
+if(isset($_POST["login"])) {
+    global $db;
+    $username = $_POST["username"];
+    $query = "SELECT * from users where username = '$username' ";
+    
+    // var_dump(mysqli_fetch_assoc(mysqli_query($db, $query)));
+    
+    $dataUserLogin = mysqli_fetch_assoc(mysqli_query($db, $query));
+    $id_user = $dataUserLogin["id_user"];
+    
+    if($dataUserLogin["username"] == $username) {
+        if(password_verify($_POST["password"], $dataUserLogin["password"])) {
+            setcookie('username', $username, time() + 3600);
+            setcookie('id_user', $id_user, time() + 3600);
+            $_SESSION["login"] = true;
+            header('Location: homepage.php');
+        } else {
+            echo"
+            <div class='alert alert-danger alert-dismissible fade show' style='z-index: 99999;' role='alert'>
+                Password Incorrect
+            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+          </div>";
+        }
+    } else {
+        echo"
+            <div class='alert alert-danger alert-dismissible fade show' style='z-index: 99999;' role='alert'>
+                Username Incorrect
+            <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+          </div>";
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/stylelogin.css">
+    <link rel="shortcut icon" href="assets/image/KN.png" type="image/x-icon">
+    <title>Kelontongers | Login Form</title>
+</head>
+<body>
+    <img src="assets/image/backgroundHome.png" alt="" id="animasi" class="animasi1">
+        <div class="kiri">
+            <div class="logo">
+                <h1>Kelonto<span>Ngers</span></h1>
+            </div>
+            <div class="opening">
+                <h1>Hello Welcome Back!</h1>
+                <h3>Please log in to your account!</h3>
+            </div>
+            <div class="formSignUp">
+                <form action="" method="post">
+                    <label for="username">Username</label>
+                    <input type="text" placeholder="example: rcmfrey7673" name="username" id="username" required><br>
+
+                    <label for="password">Password</label>
+                    <input type="password" placeholder="input your password account" id="password" name="password" required><br>
+
+                    <div class="button">
+                        <button type="submit" id="login1" name="login">Login</button>
+                        <a href="signUp.php" id="login2">Sign Up</a>
+                    </div>
+                </form>
+            </div>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+
+</body>
+</html>
---
+++ signUp.php
@@ -0,0 +1,90 @@
+<?php
+    
+    $db = mysqli_connect("localhost", "root", "", "kelontongers");
+
+    function tambahUser($username, $email, $password) {
+        global $db;
+        $salt = uniqid(mt_rand(), true);
+        $id = uniqid($salt);
+        $nameUser = htmlspecialchars($username);
+        $emailUser = htmlspecialchars($email);
+        $passwordUser = htmlspecialchars($password);
+        
+        $newPassword = password_hash($passwordUser, PASSWORD_DEFAULT);
+
+        // Mengecek apakah sudah ada username dan email sudah terdaftar atau belum
+        $usernameAndEmail = mysqli_query($db, "SELECT username, email FROM users WHERE username = '$nameUser' OR email = '$emailUser'");
+        
+        // Jika username atau email sudah terdaftar, tampilkan pesan error menggunakan pop up bootstrap
+        if(mysqli_num_rows($usernameAndEmail) > 0) {
+            echo "<div class='alert alert-danger alert-dismissible fade show' style='z-index: 99999;' role='alert'>
+                    Username or Email already exist!
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                  </div>";
+        } else {
+            $query = "INSERT INTO users
+                        values
+                        ('$id', '$nameUser', '$newPassword', '$emailUser')
+                    ";
+            mysqli_query($db, $query);
+
+            // Tampilkan pesan berhasil menggunakan pop up bootstrap
+            echo "<div class='alert alert-success alert-dismissible fade show' role='alert' style='z-index: 99999;  '>
+                    Successfully registered!
+                    <button type='button' class='btn-close' data-bs-dismiss='alert' aria-label='Close'></button>
+                  </div>";
+        }
+    }
+
+    if(isset($_POST["signUp"])) {
+        tambahUser($_POST["username"], $_POST["email"], $_POST["password"]);
+    }
+    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/image/KN.png" type="image/x-icon">
+    <title>Kelontongers | Sign Up Form</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/stylesSignUp.css">
+
+</head>
+<body>
+    <div class="kiri">
+        <img src="assets/image/backgroundHome.png" alt="" id="animasi" class="animasi1">
+    </div>
+    <div class="kanan">
+        <div class="logo">
+            <h1>Kelonton<span>Ngers</span></h1>
+        </div>
+        <div class="opening">
+            <h1 id="heading1">Hello New User!!</h1>
+            <h3 id="heading3">You Must Have An Account To Join With Us</h3>
+        </div>
+        <div class="formSignUp">
+            <form action="" method="post">
+                <label for="username">Username</label>
+                <input required type="text" placeholder="example: rcmfrey7673" name="username" id="username" ><br>
+
+                <label for="email">Email</label>
+                <input required type="email" placeholder="example: richard.marcell@gmail.com" name="email" id="email" ><br>
+
+                <label for="password">Password</label>
+                <input required type="password" placeholder="input your password account" id="password" name="password" ><br>
+
+                <div class="button">
+                    <a href="login.php" id="signUp1" class="text-decoration-none">Login</a>
+                    <button type="submit" id="signUp2" name="signUp">Sign Up</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle.min.js" integrity="sha384-qKXV1j0HvMUeCBQ+QVp7JcfGl760yU08IQ+GpUo5hlbpg51QRiuqHAJz8+BrxE/N" crossorigin="anonymous"></script>
+
+</body>
+</html>
